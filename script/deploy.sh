b7365c610b553c3c579fe0ba8d199f09ce67b476
chenjun3092 <20100688@qq.com>
2015-12-27 22:40:44 -0500

    changes nodejs-web to v2.5

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,8 +7,5 @@ else
 fi
 
 webport=$[8080+$[version]]
-#echo $[webport]
-#docker run -d --name CI-jenkins-"${version}" --privileged -p "$[webport]":8080 -v /var/lib/docker/:/var/lib/docker/ -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
 
-#docker run 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
 docker run -d --name Testing-Web-"${version}" -p "$[webport]":8080 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
