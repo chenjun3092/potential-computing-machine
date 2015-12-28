94c5acae62d30a939c2d9a12c8cf3738456ab805
chenjun3092 <20100688@qq.com>
2015-12-27 21:06:30 -0500

    changes nodejs-web to v2.3

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,7 +7,7 @@ else
 fi
 
 #docker run 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
-webport=${8080+${version}}
-#echo ${webport}
-docker run -d --name CI-jenkins-"${version}" --privileged -p "${webport}":8080 -v /var/lib/docker/:/var/lib/docker/ -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
+webport=$[8080+$[version]]
+echo $[webport]
+docker run -d --name CI-jenkins-"${version}" --privileged -p "$[webport]":8080 -v /var/lib/docker/:/var/lib/docker/ -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
 
--- src/server.js
+++ src/server.js
@@ -3,6 +3,5 @@ http.createServer( function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write('<h1>Hello jenkins!!!</h1>');
 	res.write('<h1>Has changed</h1>');
-	res.write('<h1>Has changed!!</h1>');
 	res.end();
 }).listen(8080);
