03de34d6dd4025040b3132493e2c050b9d62379f
chenjun3092 <20100688@qq.com>
2015-12-27 20:29:18 -0500

    changes nodejs-web to v2.1

--- script/build.sh
+++ script/build.sh
@@ -4,4 +4,5 @@ if [ -z "${1}" ]; then
 else
    version="${1}"
 fi
-docker build -t 192.168.4.12:80/chenjun-nodejs-jsweb:${version} ../
+
+docker build -t 192.168.4.12:80/chenjun-nodejs-jsweb:${version} .
