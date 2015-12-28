d793fbd8c14431771b83c9e4157c05d182687e44
chenjun3092 <20100688@qq.com>
2015-12-27 21:44:45 -0500

    changes nodejs-web to vlatest

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,8 +6,9 @@ else
    version="${1}"
 fi
 
-#docker run 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
 webport=$[8080+$[version]]
-echo $[webport]
-docker run -d --name CI-jenkins-"${version}" --privileged -p "$[webport]":8080 -v /var/lib/docker/:/var/lib/docker/ -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
+#echo $[webport]
+#docker run -d --name CI-jenkins-"${version}" --privileged -p "$[webport]":8080 -v /var/lib/docker/:/var/lib/docker/ -v /var/run/docker.sock:/var/run/docker.sock -v /usr/bin/docker:/usr/bin/docker 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
 
+#docker run 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
+docker run -d --name Testing-Web-"${version}" -p "$[webport]":8080 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
