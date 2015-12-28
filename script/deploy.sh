53ce1c377ccb2b6e65bc31b6c97af4901cdb6b3d
chenjun3092 <20100688@qq.com>
2015-12-28 07:43:11 -0500

    changes nodejs-web to vlatest

--- script/deploy.sh
+++ script/deploy.sh
@@ -8,4 +8,4 @@ fi
 
 webport=$[8080+$[version]]
 
-docker run -d --name Test-Web:"$[webport]" -p "$[webport]":8080 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
+docker run -d --name Test-Web-"$[webport]" -p "$[webport]":8080 192.168.4.12:80/chenjun-nodejs-jsweb:"${version}"
