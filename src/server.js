ccf78c546bc24f663c6c88f400c33004d94d19d6
chenjun3092 <chenjun@cloudsoar.com>
2016-03-19 00:58:03 -0400

    appsoar

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer( function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('<h1>Hello Appsoar Cloudsoar!!!</h1>');
+	res.write('<h1>Hello  Cloudsoar!!!</h1>');
 	res.write('<h1>Has changed!!!</h1>');
 	res.end();
 }).listen(8080);
